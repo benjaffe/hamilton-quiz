bedc9eef12738e9b19c59981cc9b0dcb24ffee04
Ben Jaffe <jaffe.ben@gmail.com>
2017-05-28 17:44:56 -0700

    ehm

--- src/Ham.js
+++ src/Ham.js
@@ -2,8 +2,8 @@ import Lyrics from './data/All_Lyrics_No_Speakers';
 import LyricsSpeakers from './data/All_Lyrics_Speakers';
 
 const words = Lyrics;
-const wordsIsolated = removePunct(words).split(' ');
-const wordsIsolatedSanitized = removePunct(Lyrics.toLowerCase()).split(' ');
+const wordsIsolated = _removePunct(words).split(' ');
+const wordsIsolatedSanitized = _removePunct(Lyrics.toLowerCase()).split(' ');
 
 String.prototype.indexOfRegex = function(regex, fromIndex){
   var str = fromIndex ? this.substring(fromIndex) : this;
@@ -17,27 +17,35 @@ String.prototype.lastIndexOfRegex = function(regex, fromIndex){
   return match ? str.lastIndexOf(match[match.length-1]) : -1;
 }
 
-function removePunct(str) {
+function _removePunct(str) {
   return str
     .replace(/[^\w\s]|_/g, ' ')
     .replace(/\s+/g, ' ');
 }
 
-function getOffsetWords(word, offset, offset2) {
+
+
+function getOffsetWords(word, offset, offset2, frequency) {
   let i = -1;
   let offsetWordCollection = [];
 
+  if (!word) console.log(arguments);
+
   do {
     i = words.toLowerCase().indexOf(word.toLowerCase(), i + 1);
-    console.log(i);
-    offsetWordCollection.push(getFirstOffsetWords(word, offset, offset2, i));
-  } while (i !== -1)
+    if (i === -1) continue;
+
+    offsetWordCollection.push(getFirstOffsetWords(word, offset, offset2, i + 1));
+  } while (i !== -1);
 
   return offsetWordCollection;
 }
 
 function getFirstOffsetWords(word, offset, offset2, startPosition) {
-  let i = words.toLowerCase().indexOf(word.toLowerCase(), startPosition);
+  console.log(word, offset, offset2, startPosition);
+
+  let regexp = new RegExp('[^\\w](' + word.toLowerCase() + ')[^\\w]', 'ig');
+  let i = words.toLowerCase().indexOfRegex(regexp, startPosition);
 
   let endIndex = i + word.length;
   while (offset2 > 0) {
@@ -46,39 +54,40 @@ function getFirstOffsetWords(word, offset, offset2, startPosition) {
   }
 
   let startIndex = i;
-  while (offset <= 0) {
+  while (offset < 0) {
     startIndex = words.lastIndexOfRegex(/\s/, startIndex);
     offset++;
   }
 
-  return words.slice(startIndex, endIndex).trim();
+  return words.slice(startIndex, endIndex + 1).trim();
 }
 
 
 
-function getOffsetWordArray(word, offset, offset2) {
-  let i = wordsIsolatedSanitized.findIndex(w => w === word.toLowerCase());
+// function getOffsetWordArray(word, offset, offset2) {
+//   let i = wordsIsolatedSanitized.findIndex(w => w === word.toLowerCase());
 
-  let words = [];
-  // previous word(s)
-  while (offset < 0) {
-    words.push(wordsIsolatedSanitized[i + offset++]);
-  }
+//   let words = [];
+//   // previous word(s)
+//   while (offset < 0) {
+//     words.push(wordsIsolatedSanitized[i + offset++]);
+//   }
 
-  // the original word
-  words.push(wordsIsolatedSanitized[i + offset++]);
+//   // the original word
+//   words.push(wordsIsolatedSanitized[i + offset++]);
 
-  // next word(s)
-  if (offset2) {
-    while (offset < offset2) {
-      words.push(wordsIsolatedSanitized[i + offset++]);
-    }
-  }
+//   // next word(s)
+//   if (offset2) {
+//     while (offset < offset2) {
+//       words.push(wordsIsolatedSanitized[i + offset++]);
+//     }
+//   }
 
-  return words;
-}
+//   return words;
+// }
 
 function getWordsWithFrequency(num) {
+  console.log('getting words with frequency ' + num);
   let counts = getWordsCounts();
   return Object.keys(counts).filter(key => counts[key] === num);
 }
@@ -101,6 +110,6 @@ export default {
   getLyrics,
   getWordsWithFrequency,
   getWordsCounts,
-  getOffsetWordArray,
+  // getOffsetWordArray,
   getOffsetWords,
 };
--- src/Quiz.js
+++ src/Quiz.js
@@ -8,17 +8,23 @@ function addEllipses(str) {
 class Quiz extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      word: this.nextWord(),
-      wordsBefore: 0,
-      wordsAfter: 0,
-    };
 
     this.handleClick = this.handleClick.bind(this);
+    this.nextWord = this.nextWord.bind(this);
+
+    this.state = {
+      word: 'pro',
+      wordsBefore: 0,
+      wordsAfter: 0,
+      wordFrequency: 1,
+    };
+
+    // this.state.word = this.nextWord();
   }
 
   nextWord() {
-    let words = Ham.getWordsWithFrequency(1);
+    let words = Ham.getWordsWithFrequency(this.state.wordFrequency);
+    console.log('words are ' + words);
     let i = Math.floor(Math.random() * words.length);
     return words[i];
   }
@@ -31,7 +37,7 @@ class Quiz extends Component {
 
   handleChange(name, event) {
     var obj = {};
-    obj[name] = event.target.value;
+    obj[name] = event.target.value * 1;
     this.setState(prevState => obj);
   }
 
@@ -40,26 +46,41 @@ class Quiz extends Component {
       <div className="Quiz">
         <button onClick={this.handleClick}>Next Word</button>
         <br />
+        <p>Word Frequency: {this.state.wordFrequency}</p>
+        <input
+           type="range"
+           value={this.state.wordFrequency}
+           min="1"
+           max="20"
+           onChange={this.handleChange.bind(this, "wordFrequency")}
+           step="1" />
+        <p>{this.state.wordsBefore} Words Before</p>
         <input
            type="range"
            value={this.state.wordsBefore}
            min="0"
            max="10"
-           onInput={this.handleChange.bind(this, "wordsBefore")}
+           onChange={this.handleChange.bind(this, "wordsBefore")}
            step="1" />
-        <p>{this.state.wordsBefore}</p>
+        <p>{this.state.wordsAfter} Words After</p>
         <input
            type="range"
            value={this.state.wordsAfter}
            min="0"
            max="10"
-           onInput={this.handleChange.bind(this, "wordsAfter")}
+           onChange={this.handleChange.bind(this, "wordsAfter")}
            step="1" />
-        <p>{this.state.wordsAfter}</p>
         <h2>{this.state.word}</h2>
         <pre className="App-intro">
           {
-            Ham.getOffsetWords(this.state.word, this.state.wordsBefore * -1, this.state.wordsAfter).map(addEllipses).join('\n\n')
+            Ham
+              .getOffsetWords(
+                this.state.word,
+                this.state.wordsBefore * -1,
+                this.state.wordsAfter,
+              )
+              .map(addEllipses)
+              .join('\n\n')
           }
         </pre>
       </div>
